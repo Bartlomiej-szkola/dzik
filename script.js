ef18bd6051214eb145e13d53eb91a22b58527ed8
Bartlomiej Tobiasz <bartlomiej.tobiasz@tm1.edu.pl>
2024-10-26 12:06:31 +0200

    Podstawowe dzialanie

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 const h1Liczba = document.getElementById('h1Liczba')
-var numer = 0
-var start = Date.now();
-var ostatniaLiczba = Date.now();
+let numer = 0
+const start = Date.now()
+let ostatniaLiczba = Date.now()
+let czyNalezyKliknac = false
+let czyKlikniętoDzika = false
 
 function kolejnyNumer(){
+    if(czyNalezyKliknac && !czyKlikniętoDzika){
+        alert("Nie kliknięto dzika")
+        clearInterval(numerInterval)
+    }
+
     numer++
     ostatniaLiczba = Date.now();
+    czyKlikniętoDzika = false;
+    czyNalezyKliknac = czyMa7(numer)
     h1Liczba.innerHTML = numer
-    if(numer%7 == 0 || numer.toString().split("").includes('7')){
-        console.log("a")
-    }
 }
-setInterval(kolejnyNumer, 1000)
+const numerInterval = setInterval(kolejnyNumer, 500)
 
 function dzik(){
-    alert(czasReakcji)
+    const audio = new Audio('dzik1.wav');
+    audio.play();
+    if(czyNalezyKliknac){
+        console.log(czasReakcji())
+        czyKlikniętoDzika = true
+    }
+    else{
+        alert("W tym numerze nie nalezalo kliknac dzika")
+        clearInterval(numerInterval)
+    }
 }
 
+function playAudio(url) {
+    new Audio(url).play();
+  }
+
+function czyMa7(numer){
+    if(numer%7 == 0 || numer.toString().split("").includes('7')){
+        return true
+    }
+    return false
+}
 
 
 function czasReakcji(){
     let delta = Date.now() - ostatniaLiczba;
-    return delta;
+    return delta
 }
 
 // setInterval(function() {
